f2e3bba7970eff991a1696598fb7294d6f4d68d6
albertkrs <114407183+albertkrs@users.noreply.github.com>
2022-09-29 12:38:44 +0200

    first upload

---
+++ login.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="main.css">
+</head>
+
+<body>
+    <section class="landing">
+        <div class="form-container">
+            <form action="" method="post" >
+
+                <h3>LOGIN</h3>
+                <input type="text" placeholder="Email" name="email" class="input">
+         
+                <input type="text" placeholder="Passowrd" name="password" class="input">
+                
+                <button name="submit" class="btn">Login</button>
+
+                <p>you don't have an account ? <a href="">Register here</a></p>
+             
+            </form>
+        </div>
+
+    </section>
+</body>
+
+</html>
---
+++ register.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="main.css">
+</head>
+
+<body>
+    <section class="landing">
+        <div class="form-container">
+            <form action="" method="post" >
+
+                <h3>REGISTER</h3>
+                <input type="text" placeholder="Username" name="username" class="input" required>
+
+                <input type="text" placeholder="Email" name="email" class="input" required>
+         
+                <input type="text" placeholder="Passowrd" name="password" class="input" required>
+                <input type="text" placeholder="Confirm Passowrd" name="cpassword" class="input" required>
+                
+                <button name="submit" class="btn">REGISTER</button>
+
+                <p>you aleready have an account ? <a href="">login here</a></p>
+             
+            </form>
+        </div>
+
+    </section>
+</body>
+
+</html>
