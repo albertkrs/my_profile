babf60efd1b4cf9f7e2871f499d80e03b78198ac
albertkrs <114407183+albertkrs@users.noreply.github.com>
2022-09-29 14:58:51 +0200

    Update register.php

--- register.php
+++ register.php
@@ -1,3 +1,77 @@
+
+<?php
+@include 'config.php';
+ error_reporting(0);
+
+if (isset($_POST['submit'])){
+  $username=$_POST['username'];
+  $email=$_POST['email'];
+  $password=md5($_POST['password']);
+  $cpassword=md5($_POST['cpassword']);
+  
+  if($password!=$cpassword) {
+ 
+          echo "<script>alert('password not matched');</script>";
+        }
+
+ else {  
+    ///verification email 
+            $sql="SELECT *FROM user WHERE email='$email' AND password='$password'";
+           $result=mysqli_query($connect,$sql);
+            if (mysqli_num_rows($result)>0){
+                echo "<script>alert('email deja exist');</script>";
+            }
+              
+ 
+
+  else {
+ ///inser in database
+    $sql="INSERT INTO user(username,email,password) 
+  VALUES('$username','$email','$password')";
+    $result=mysqli_query($connect,$sql);
+
+    if ($result) {
+     
+        echo "<script>alert('Woow  Wser Registration complete  ');</script>";}
+    else  
+    echo "<script>alert('Wooops  something Wrong');</script>"; 
+    }
+      
+    }
+}
+ 
+
+
+
+
+
+
+
+  
+
+
+
+
+
+
+
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 
